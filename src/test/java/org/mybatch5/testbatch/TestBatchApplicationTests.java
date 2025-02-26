31d6af48bf80a674915e0d540e631c4b59516ee2
7dong7 <tmdkdl777@gmail.com>
2025-02-26 16:24:27 +0900

    스프링 배치5 프로젝트 생성

---
+++ src/test/java/org/mybatch5/testbatch/TestBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mybatch5.testbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
